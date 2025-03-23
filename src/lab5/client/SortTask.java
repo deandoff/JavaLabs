7f02215cf4de774b57c0e387a3d7dbb34e3f46c3
deandoff <deandoff@gmail.com>
2025-03-23 19:46:20 +0300

    lab5 release

--- src/lab5/client/SortTask.java
+++ src/lab5/client/SortTask.java
@@ -8,6 +8,7 @@ import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.util.Arrays;
 import java.util.List;
+import java.util.ArrayList;
 
 public class SortTask implements Task<List<Integer>>, Serializable {
     private final List<Integer> numbers;
@@ -24,13 +25,26 @@ public class SortTask implements Task<List<Integer>>, Serializable {
     }
 
     public static void main(String[] args) {
+        if (args.length < 3) {
+            System.err.println("Usage: java lab5.client.SortTask <host> <port> <number1> <number2> ...");
+            System.exit(1);
+        }
+
         try {
-            String name = "Compute";
-            Registry registry = LocateRegistry.getRegistry(args[0]);
-            Compute comp = (Compute) registry.lookup(name);
-            List<Integer> numbers = Arrays.asList(5, 3, 9, 1, 6, 2, 8, 4, 7);
-            SortTask task = new SortTask(numbers);
+            String host = args[0];
+            int port = Integer.parseInt(args[1]);
+
+            List<Integer> numbers = new ArrayList<>();
+            for (int i = 2; i < args.length; i++) {
+                numbers.add(Integer.parseInt(args[i]));
+            }
+
+            Registry registry = LocateRegistry.getRegistry(host, port);
+            Compute comp = (Compute) registry.lookup("Compute");
+
+            Task<List<Integer>> task = new SortTask(numbers);
             List<Integer> sortedNumbers = comp.execute(task);
+
             System.out.println("Sorted numbers: " + sortedNumbers);
         } catch (Exception e) {
             System.err.println("ComputeClient exception:");
